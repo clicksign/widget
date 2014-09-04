9714dcef8e837bb972051361860c68f38406b944
Daniel Libanori <daniellibanori@gmail.com>
2014-09-04 15:24:56 -0300

    Add create signature list

--- example/api-client.rb
+++ example/api-client.rb
@@ -13,6 +13,16 @@ configure do
 end
 
 helpers do
+  def materialize(tempname, tempfile)
+    filename = "tmp/#{tempname}"
+    File.open(filename, "wb") { |file| file.write(tempfile.read) }
+    file = File.open(filename, "rb")
+
+    yield(file)
+  ensure
+    File.delete(filename)
+  end
+
   def document(json)
     OpenStruct.new(json).tap do |document|
       list = OpenStruct.new(document.list)
@@ -31,52 +41,82 @@ helpers do
     { access_token: settings.access_token }.merge(params)
   end
 
-  def get_documents
+  def api_get_documents
     RestClient.get(api_url, params: api_params, accept: "json")
   end
 
-  def get_document(key)
+  def api_get_document(key)
     RestClient.get(api_url(key), params: api_params, accept: "json")
   end
 
-  def create_document(file)
-    RestClient.post(api_url, api_params("document[archive][original]" => file), accept: "json")
+  def api_create_document(name, temp)
+    materialize(name, temp) do |file|
+      RestClient.post(api_url, api_params("document[archive][original]" => file), accept: "json")
+    end
+  end
+
+  def api_create_list(key, signers)
+    RestClient.post(api_url(key, 'list') + "?access_token=#{settings.access_token}", { signers: signers }.to_json, content_type: "json", accept: "json")
+  end
+
+  def get_documents
+    JSON[api_get_documents].collect { |r| document(r['document']) }
+  end
+
+  def get_document(key)
+    document(JSON[api_get_document(key)]['document'])
+  end
+
+  def create_document(name, temp)
+    document(JSON[api_create_document(name, temp)]['document'])
+  end
+
+  def create_list(key, signers)
+    document(JSON[api_create_list(key, signers)]['document'])
   end
 end
 
 # Index all documents related to access token
 get "/" do
-  @documents = JSON[get_documents].collect { |record| document(record['document']) }
-
+  @documents = get_documents
   haml :index
 end
 
 # Upload a document
 post "/" do
-  filename = "tmp/#{params[:archive][:filename]}"
-  filetemp = params[:archive][:tempfile]
-  File.open(filename, "wb") { |file| file.write(filetemp.read) }
+  name = params[:archive][:filename]
+  temp = params[:archive][:tempfile]
 
   begin
-    file = File.open(filename, "rb")
-    @document = document(JSON[create_document(file)]['document'])
+    @document = create_document(name, temp)
   rescue
     redirect to("/"), 500
   else
     redirect to("/#{@document.key}"), 303
-  ensure
-    File.delete(filename)
   end
 end
 
 # Show a specific document
 get %r{/(\h{4}-\h{4}-\h{4}-\h{4})} do |key|
-  @document = document(JSON[get_document(key)]['document'])
-
+  @document = get_document(key)
   haml :show
 end
 
+# Create signature list
+post %r{/(\h{4}-\h{4}-\h{4}-\h{4})/list} do |key|
+  emails = params[:emails].each_line.collect { |line| line.chomp.split(",") }
+  signers = emails.collect { |email, act| { email: email, act: act }}
+
+  begin
+    @document = create_list(key, signers)
+  # rescue
+  #   redirect to("/#{key}"), 500
+  else
+    redirect to("/#{@document.key}"), 303
+  end
+end
+
 # Show a document widget
-post "/widget" do
+post %r{/(\h{4}-\h{4}-\h{4}-\h{4})/widget} do |key|
   haml :widget
 end
