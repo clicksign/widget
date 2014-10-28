9df3e0bb05b70fd207ed8eb99e8fd0d3260dae56
Daniel Libanori <daniellibanori@gmail.com>
2014-10-28 16:32:54 -0200

    Use clicksign gem

--- examples/api-client.rb
+++ examples/api-client.rb
@@ -5,6 +5,7 @@ require 'coffee_script'
 require 'rest_client'
 require 'json'
 require 'ostruct'
+require 'clicksign'
 
 KEY = /(\h{8}-\h{4}-\h{4}-\h{4}-\h{12}|\h{4}-\h{4}-\h{4}-\h{4})/
 
@@ -14,6 +15,11 @@ configure do
   set :host, ENV['HOST'] || "clicksign-demo.com"
   set :api_host, "api.#{settings.host}"
   set :widget_host, "widget.#{settings.host}"
+
+  Clicksign.configure do |config|
+    config.token = settings.access_token
+    config.endpoint = "#{settings.protocol}://#{settings.api_host}"
+  end
 end
 
 helpers do
@@ -26,91 +32,30 @@ helpers do
   ensure
     File.delete(filename)
   end
-
-  def document(json)
-    OpenStruct.new(json).tap do |document|
-      list = OpenStruct.new(document.list)
-      document.list = list
-
-      signatures = document.list.signatures.collect { |s| OpenStruct.new(s) }
-      list.signatures = signatures
-    end
-  end
-
-  def api_domain
-    "#{settings.protocol}://#{settings.api_host}"
-  end
-
-  def api_url(*path)
-    ([api_domain, "v1", "documents"] + path).join("/")
-  end
-
-  def api_params(params = {})
-    { access_token: settings.access_token }.merge(params)
-  end
-
-  def api_get_documents
-    RestClient.get(api_url, params: api_params, accept: "json")
-  end
-
-  def api_get_document(key)
-    RestClient.get(api_url(key), params: api_params, accept: "json")
-  end
-
-  def api_create_document(name, temp)
-    materialize(name, temp) do |file|
-      RestClient.post(api_url, api_params("document[archive][original]" => file), accept: "json")
-    end
-  end
-
-  def api_create_list(key, signers)
-    RestClient.post(api_url(key, 'list') + "?access_token=#{settings.access_token}", { signers: signers, skip_email: true }.to_json, content_type: "json", accept: "json")
-  end
-
-  def get_documents
-    JSON[api_get_documents].collect { |r| document(r['document']) }
-  end
-
-  def get_document(key)
-    document(JSON[api_get_document(key)]['document'])
-  end
-
-  def create_document(name, temp)
-    document(JSON[api_create_document(name, temp)]['document'])
-  end
-
-  def create_list(key, signers)
-    document(JSON[api_create_list(key, signers)]['document'])
-  end
-end
-
-get "/stat" do
-  "ok"
 end
 
 # Index all documents related to access token
 get "/" do
-  @documents = get_documents
+  @documents = Clicksign::Document.all.collect { |hash| hash['document'] }
   haml :index
 end
 
+require 'pry'
 # Upload a document
 post "/" do
   name = params[:archive][:filename]
   temp = params[:archive][:tempfile]
 
-  begin
-    @document = create_document(name, temp)
-  rescue
-    redirect to("/"), 500
-  else
-    redirect to("/#{@document.key}"), 303
+  materialize(name, temp) do |file|
+    @document = Clicksign::Document.create(file)
   end
+
+  redirect to("/#{@document['key']}"), 303
 end
 
 # Show a specific document
 get %r{/#{KEY}$} do |key|
-  @document = get_document(key)
+  @document = Clicksign::Document.find(key)['document']
   haml :show
 end
 
@@ -119,13 +64,8 @@ post %r{/#{KEY}/list$} do |key|
   emails = params[:emails].each_line.collect { |line| line.chomp.split(",") }
   signers = emails.collect { |email, act| { email: email, act: act }}
 
-  begin
-    @document = create_list(key, signers)
-  rescue
-    redirect to("/#{key}"), 500
-  else
-    redirect to("/#{@document.key}"), 303
-  end
+  @document = Clicksign::Document.create_list(key, signers, false)['document']
+  redirect to("/#{@document['key']}"), 303
 end
 
 # Show a document widget
